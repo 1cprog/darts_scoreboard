102f4baa7665283745803cd79811fd1cc77b6458
Sasin Paul <figuransk@gmail.com>
2019-12-13 10:11:44 +0700

    add player class

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 // Main elements
-const gameSetting = {
+class Player {
+    constructor(index, name) {
+        this.playerName = document.querySelector(`.helper.player${index + 1} .player_name`);
+        this.scoresPlayer = document.querySelector(`.player${index + 1}.scores_left`);
+        this.dartsThrown = document.querySelector(`.player${index + 1}.darts_thrown`);
+        this.legsWin = document.querySelector(`.player${index + 1}.win_legs`);
+        this.name = name;
+        this.wins = 0;
+        this.darts = 0;
+        this.score = 501;
+    }
+}
 
-};
-
-const scoresPlayer1 = document.querySelector(`.player1.scores_left`);
-const dartsThrown = document.querySelector('.player1.darts_thrown');
-const legsWin = document.querySelector('.player1.win_legs');
-let wins = 0;
-let darts = 0;
-var score = 501;
-
-// Init
-scoresPlayer1.innerText = score;
-dartsThrown.innerText = darts;
-legsWin.innerText = wins;
+const players = [];
 
 const calcTrhow = (event) => {
     event.preventDefault();
@@ -34,8 +33,19 @@ const calcTrhow = (event) => {
     document.querySelector('input').value = '';
 };
 
-const newGame = () => {
-
+// Init
+const newGame = (event) => {
+    const inputs = [...document.querySelectorAll('input[id^="player"]')]
+    for (let i = 0; i < inputs.length; i++) {
+        let newPlayer = new Player(i, inputs[i].value);
+        players.push(newPlayer);
+        newPlayer.playerName.innerText = newPlayer.name;
+        newPlayer.scoresPlayer.innerText = newPlayer.score;
+        newPlayer.dartsThrown.innerText = newPlayer.darts;
+        newPlayer.legsWin.innerText = newPlayer.wins;
+    }
+    const playersInput = document.querySelector('.container_input');
+    playersInput.style.display = 'none';
 };
 
 const okButton = document.querySelector('button');
@@ -47,7 +57,6 @@ const initPlayers = () => {
     playersInput.style.display = 'block';
     const button = document.querySelector('input.btn');
     button.addEventListener('click', newGame);
-    button.addEventListener('keyup', newGame);
 };
 
 window.onload = () => {
